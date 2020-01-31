5984dc329387fb54125014188d623e5ac8dd44bc
Qiang Yiting <qiangyt@wxcount.com>
2020-01-31 15:31:14 +0800

    improve sold-out notification

--- server/src/main/java/qiangyt/springboot_example/server/service/Provisioner.java
+++ server/src/main/java/qiangyt/springboot_example/server/service/Provisioner.java
@@ -1,16 +1,19 @@
 package qiangyt.springboot_example.server.service;
 
-import java.util.UUID;
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import qiangyt.springboot_example.server.entity.AccountEO;
-import qiangyt.springboot_example.server.entity.OrderEO;
-import qiangyt.springboot_example.server.entity.ProductEO;
+import qiangyt.springboot_example.api.AccountAPI;
+import qiangyt.springboot_example.api.OrderAPI;
+import qiangyt.springboot_example.api.ProductAPI;
+import qiangyt.springboot_example.api.rnr.CreateAccountReq;
+import qiangyt.springboot_example.api.rnr.CreateOrderReq;
+import qiangyt.springboot_example.api.rnr.CreateProductReq;
+import qiangyt.springboot_example.api.vo.Account;
+import qiangyt.springboot_example.api.vo.Product;
 import qiangyt.springboot_example.server.repository.AccountRepository;
 import qiangyt.springboot_example.server.repository.OrderRepository;
 import qiangyt.springboot_example.server.repository.ProductRepository;
@@ -27,58 +30,57 @@ import lombok.Getter;
 public class Provisioner {
 
     @Autowired
+    private AccountAPI accountAPI;
+
+    @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private ProductAPI productAPI;
+    
     @Autowired
     private ProductRepository productRepository;
 
     @Autowired
+    private OrderAPI orderAPI;
+
+    @Autowired
     private OrderRepository orderRepository;
 
-    @Autowired
-    private OrderService orderService;
-
-    @Autowired
-    private ProductService productService;
 
     @PostConstruct
     public void init() {
-        AccountEO c1;
+        Account c1;
         if (getAccountRepository().count() == 0) {
-            c1 = new AccountEO();
-            c1.setAddress("ca");
-            c1.setFirstName("fn");
-            c1.setSecondName("sn");
-            c1.setId(UUID.randomUUID());
-            c1 = getAccountRepository().save(c1);
-
+            var req = new CreateAccountReq();
+            req.setAddress("ca");
+            req.setFirstName("fn");
+            req.setSecondName("sn");
+            req.setPassword("pwd");
+            c1 = getAccountAPI().createAccount(req);
         } else {
-            c1 = getAccountRepository().findAll().iterator().next();
+            var accountId = getAccountRepository().findAll().iterator().next().getId();
+            c1 = getAccountAPI().loadAccount(accountId);
         }
 
-        ProductEO p1;
+        Product p1;
         if (getProductRepository().count() == 0) {
-            p1 = new ProductEO();
-            p1.setAmount(99);
-            p1.setName("pn");
-            p1.setId(UUID.randomUUID());
-            p1 = getProductRepository().save(p1);
-
+            var req = new CreateProductReq();
+            req.setAmount(99);
+            req.setName("pn");
+            p1 = getProductAPI().createProduct(req);
         } else {
-            p1 = getProductRepository().findAll().iterator().next();
+            var productId = getProductRepository().findAll().iterator().next().getId();
+            p1 = getProductAPI().loadProduct(productId);
         }
 
-        OrderEO o1;
         if (getOrderRepository().count() == 0) {
-            o1 = new OrderEO();
-            o1.setAmount(3);
-            o1.setCustomerAccount(c1);
-            o1.setProduct(p1);
-            o1.setId(UUID.randomUUID());
+            var req = new CreateOrderReq();
+            req.setAmount(3);
+            req.setCustomerAccountId(c1.getId());
+            req.setProductId(p1.getId());
 
-            o1 = getOrderRepository().save(o1);
-        } else {
-            o1 = getOrderRepository().findAll().iterator().next();
+            getOrderAPI().createOrder(req);
         }
     }
 
